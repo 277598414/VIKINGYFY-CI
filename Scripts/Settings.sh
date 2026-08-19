7b13f65e4188035a052a9f98fb6d71b7f556eb1c
277598414 <277598414@qq.com>
2026-08-19 10:16:37 +0800

    Refactor Settings.sh for no WiFi configuration
    Remove NSS related configurations and add support for no WiFi adjustments.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -60,12 +60,9 @@ fi
 #高通平台调整
 DTS_PATH="./target/linux/qualcommax/dts/"
 if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
-	#取消nss相关feed
-	echo "CONFIG_FEED_nss_packages=n" >> ./.config
-	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=y" >> ./.config
-	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=n" >> ./.config
-	#其他调整
-	#echo "CONFIG_PACKAGE_kmod-usb-serial-qualcomm=y" >> ./.config
+	#无WIFI配置调整Q6大小
+	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
+		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
+		echo "qualcommax set up nowifi successfully!"
+	fi
 fi
